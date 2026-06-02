2ad5b1386148945c831b3a6a3a3643d566e2e608
Mikhail Koloskov <mkoloskov1@gmail.com>
2026-06-02 18:48:23 +0500

    desigion_V3

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,49 +1,45 @@
 package web.dao;
 
 import org.springframework.stereotype.Repository;
-
-import org.springframework.transaction.annotation.Transactional;
 import web.entity.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
+
     @Override
     public void save(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional(readOnly = true)
+
     @Override
     public List<User> findAll() {
         return entityManager.createQuery("select u from User u", User.class).getResultList();
     }
 
-    @Transactional
+
     @Override
     public Optional<User> findById(Long id) {
         User user = entityManager.find(User.class, id);
         return Optional.ofNullable(user);
     }
 
-    @Transactional
+
     @Override
     public void updateUser(User user) {
         entityManager.merge(user);
     }
 
-    @Transactional
+
     @Override
     public void deleteById(Long id) {
         Optional<User> userById = findById(id);
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package web.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.entity.User;
-import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -16,26 +18,31 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
+    @Transactional
     @Override
     public void save(User user) {
         userDao.save(user);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<User> findAll() {
         return userDao.findAll();
     }
 
+    @Transactional
     @Override
     public Optional<User> findById(Long id) {
         return userDao.findById(id);
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
+    @Transactional
     @Override
     public void deleteById(Long id) {
         userDao.deleteById(id);
